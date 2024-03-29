70377a7301e8acd168e00044d0108d14af8b9ff8
Louis Lecouturier <71454059+LouisLecouturier@users.noreply.github.com>
2024-03-29 13:37:28 +0100

    fixed issue

--- src/kinected/asgi.py
+++ src/kinected/asgi.py
@@ -10,17 +10,17 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 import os
 
 from channels.auth import AuthMiddlewareStack
-from channels.security.websocket import AllowedHostsOriginValidator
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from ..swipes.routing import websocket_urlpatterns
+from swipes.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kinected.settings")
 
-application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
-    "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
-    ,
-
-})
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+    }
+)
--- src/swipes/consumers.py
+++ src/swipes/consumers.py
@@ -1,7 +1,7 @@
-from channels.generic.websocket import WebsocketConsumer
-from channels.layers import get_channel_layer
-from asgiref.sync import async_to_sync
 import json
+
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
 
 
 class SwipeConsumer(WebsocketConsumer):
