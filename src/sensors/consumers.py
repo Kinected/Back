9347e93648cf6f2f0d93f148ffd04e6d7446a61c
Sam-Sponcy <93118296+Sam-Sponcy@users.noreply.github.com>
2024-04-08 12:12:45 +0200

    work
    add sensorsdata basez

--- src/sensors/consumers.py
+++ src/sensors/consumers.py
@@ -2,6 +2,7 @@ import json
 
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from .models import SensorsData
 
 class SensorsConsumer(WebsocketConsumer):
     def connect(self):
@@ -22,7 +23,14 @@ class SensorsConsumer(WebsocketConsumer):
                 "type": "Sensors_message",
                 "text": json.dumps(text_data_json)
             })
+        print(text_data_json)
+        temperature = text_data_json.get('temperature')
+        humidity = text_data_json.get('humidity')
+        luminosity = text_data_json.get('luminosity')
+        data = SensorsData(temperature=temperature, humidity=humidity, luminosity=luminosity)
+        data.save()
 
+    
     # This method will receive messages from the group
     def Sensors_message(self, event):
         self.send(text_data=event["text"])
